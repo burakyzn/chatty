9c070554fef1a26ccae3da6b8f99b2e38a60efdf
Burak Yazan <brkyznn@gmail.com>
2021-05-01 23:43:16 +0300

    online kullanicilarin gorunmesi saglandi, kullanicilarin cikisinde chat bilgilendirme mesaji eklendi

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 5000;
 
+app.use('/src', express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -14,22 +16,21 @@ var users = [];
 var nicknames = [];
 
 io.on('connection', (socket) => {
-  // sadece connection olan kullaniciya ozel yayilim
   io.to(socket.id).emit('chat message', {"user": "system", "text" : 'Chat sistemine hoşgeldin.'});
 
   socket.on('newuser', function (name) {
-    console.log(nicknames.indexOf(name));
     if(nicknames.indexOf(name) == -1){
       users[socket.id] = name;
       nicknames.push(name);
-      // kullaniciya belirledigi kullanici adini iletiyoruz 
       io.to(socket.id).emit('chat message',{"user": "system", "text" : 'Kullanıcı adını ' + name + ' olarak belirledin.'})
+      io.emit('onlineusers', {...users});
     } else {
       io.to(socket.id).emit('nicknameerror',{"user": "system", "text" : 'Bu kullanıcı adı daha once secilmis!'})
     }
 
-    // baglanti koptugunda kullaniciyi sil
     socket.once('disconnect', function () {
+      io.emit('onlineusers', {...users});
+      io.emit('chat message', {"user": "system", "text" : users[socket.id]+ " ayrıldı."});
       delete nicknames[nicknames.indexOf(users[socket.id])];
       delete users[socket.id];
     });
