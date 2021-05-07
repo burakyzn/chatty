b1be8a98986f32c458a3278a0042e54d174af949
Burak Yazan <brkyznn@gmail.com>
2021-05-07 10:17:36 +0300

    socket index.js e tasindi app.js kaldirildi main screen eklendi

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import MainScreen from './screen/main';
+import {io} from 'socket.io-client';
+
+const socket = io("http://localhost:5000"); // http://localhost:5000
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainScreen></MainScreen>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export {socket};
