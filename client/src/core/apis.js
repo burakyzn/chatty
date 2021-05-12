34b8c1930b7a383f127383d49e92fcce5bac1ffe
Burak Yazan <brkyznn@gmail.com>
2021-05-13 02:17:38 +0300

    public mesajlar ve private mesajlar icin api adresleri eklendi

--- client/src/core/apis.js
+++ client/src/core/apis.js
@@ -1,3 +1,5 @@
 export const SET_NICKNAME = '/user/setNickname';
 export const SET_AVATAR_IMG = '/user/setAvatar';
 export const GET_ROOM_LIST = '/user/getRoomList';
+export const GET_PUBLIC_MESSAGE_LIST = '/message/getPublicMessageList';
+export const GET_PRIVATE_MESSAGE_LIST = '/message/getPrivateMessageList';
