e443d2f1b8d470c5b3923a49efece5e01c969e9b
Burak Yazan <brkyznn@gmail.com>
2021-05-13 11:39:12 +0300

    getRoomMessageList api adresi eklendi

--- client/src/core/apis.js
+++ client/src/core/apis.js
@@ -3,3 +3,4 @@ export const SET_AVATAR_IMG = '/user/setAvatar';
 export const GET_ROOM_LIST = '/user/getRoomList';
 export const GET_PUBLIC_MESSAGE_LIST = '/message/getPublicMessageList';
 export const GET_PRIVATE_MESSAGE_LIST = '/message/getPrivateMessageList';
+export const GET_ROOM_MESSAGE_LIST = '/message/getRoomMessageList';
