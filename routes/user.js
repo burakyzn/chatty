675a5a7567456b0141722fec2b810489b887df71
Berkay Şahin <berkayshn97@gmail.com>
2021-05-14 16:26:51 +0300

    apiye kullanici kaydi yolu belirtildi

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 const userController = require('../controllers/user'); 
 
-router.get('/setNickname', userController.setNickname);
+router.post('/register', userController.register);
 router.post('/setAvatar', userController.setAvatar); 
 router.get('/getRoomList', userController.getRoomList);
 
