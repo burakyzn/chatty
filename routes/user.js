149314e790da4c024df30c6078de2f91a6924943
Berkay Şahin <berkayshn97@gmail.com>
2021-05-14 22:10:05 +0300

    api ye auth dogrulama yolu eklendi

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router  = express.Router();
 const userController = require('../controllers/user'); 
 
 router.post('/register', userController.register);
+router.post('/authVerify', userController.authVerify);
 router.post('/setAvatar', userController.setAvatar); 
 router.get('/getRoomList', userController.getRoomList);
 
