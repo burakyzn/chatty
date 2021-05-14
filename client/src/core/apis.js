63b93411c7dc6f06bbe65e0e41f7f622bec330be
Berkay Şahin <berkayshn97@gmail.com>
2021-05-14 16:30:56 +0300

    clienta kullanici kaydi server api yolu belirtildi

--- client/src/core/apis.js
+++ client/src/core/apis.js
@@ -1,4 +1,4 @@
-export const SET_NICKNAME = '/user/setNickname';
+export const REGISTER = '/user/register';
 export const SET_AVATAR_IMG = '/user/setAvatar';
 export const GET_ROOM_LIST = '/user/getRoomList';
 export const GET_PUBLIC_MESSAGE_LIST = '/message/getPublicMessageList';
