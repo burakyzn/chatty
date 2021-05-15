f88077d4e582b86063324cafb87afcbe8be59b86
Burak Yazan <brkyznn@gmail.com>
2021-05-15 15:21:32 +0300

    import firebase key kismi canlida hata vermemesi icin duzeltildi

--- client/src/core/db.js
+++ client/src/core/db.js
@@ -1,21 +1,18 @@
-import firebase from 'firebase'
-import key from './serviceAccountKey.js'
+import firebase from 'firebase';
 let fire;
 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   fire = firebase.initializeApp({
-      "apiKey": process.env.FIREBASE_CL_API_KEY,
-      "authDomain": process.env.FIREBASE_CL_AUTH_DOMAIN,
-      "projectId": process.env.FIREBASE_CL_PROJECT_ID,
-      "storageBucket": process.env.FIREBASE_CL_STORAGE_BUCKET,
-      "messagingSenderId": process.env.FIREBASE_CL_MESSAGING_SENDER_ID,
-      "appId": process.env.FIREBASE_CL_APP_ID
+    apiKey: process.env.FIREBASE_CL_API_KEY,
+    authDomain: process.env.FIREBASE_CL_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_CL_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_CL_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_CL_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_CL_APP_ID,
   });
 } else {
-  var serviceAccount = key;
-
-  fire =  firebase.initializeApp(serviceAccount);
+  const key = require('./serviceAccountKey');
+  fire = firebase.initializeApp(key.default);
 }
 
-// const db = firebase.auth();
 export default fire;
