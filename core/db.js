d91d2db783603af1edf1b89a08a346885b72ed19
Burak Yazan <brkyznn@gmail.com>
2021-05-16 16:48:26 +0300

    firebase private key bug

--- core/db.js
+++ core/db.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
       type: process.env.FIREBASE_TYPE,
       project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: process.env.FIREBASE_AUTH_URI,
