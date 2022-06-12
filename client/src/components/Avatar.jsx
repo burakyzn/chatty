d0bcfab940a688e209f54b3500d4eff10fc784c2
Burak Yazan <brkyznn@gmail.com>
2022-06-12 15:18:39 +0300

    add default avatar

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -1,7 +1,11 @@
 import "../styles/Avatar.css";
 
 export default function Avatar(props) {
-  const { src, className } = props;
+  const { src, className, text } = props;
 
-  return <img src={src} className={`avatar ${className}`}></img>;
+  return src ? (
+    <img src={src} className={`avatar ${className}`} alt=""></img>
+  ) : (
+    <div className={`avatar default ${className}`}>{text[0]}</div>
+  );
 }
