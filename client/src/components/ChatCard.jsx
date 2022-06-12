8f942476d6b7ca9c4ca1a1f807974e6207ac02a5
Burak Yazan <brkyznn@gmail.com>
2022-06-12 15:19:04 +0300

    add socket listeners for online and offline users

--- client/src/components/ChatCard.jsx
+++ client/src/components/ChatCard.jsx
@@ -2,14 +2,20 @@ import { default as CircleIcon } from "@mui/icons-material/Circle";
 import Avatar from "../components/Avatar";
 import "../styles/ChatCard.css";
 
-export default function ChatCard() {
+export default function ChatCard(props) {
+  const { text, avatarSrc, hiddenCircle, online } = props;
+
   return (
     <div className="chat-card">
       <div className="chat-card__avatar">
-        <Avatar src="https://randomuser.me/api/portraits/men/16.jpg" />
+        <Avatar src={avatarSrc} text={text} />
       </div>
-      <div className="chat-card__text">Burak Yazan</div>
-      <CircleIcon className="chat-card__circle green" />
+      <div className="chat-card__text">{text}</div>
+      {!hiddenCircle && (
+        <CircleIcon
+          className={`chat-card__circle ${online ? "green" : "grey"}`}
+        />
+      )}
     </div>
   );
 }
--- client/src/components/ChatList.jsx
+++ client/src/components/ChatList.jsx
@@ -1,11 +1,52 @@
+import { useState, useEffect, useContext } from "react";
+import { SocketContext } from "../contexts/socketContext";
+import { Avatar } from "@mui/material";
 import ChatCard from "./ChatCard";
 import "../styles/ChatList.css";
 
 export default function ChatList() {
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [offlineUsers, setOfflineUsers] = useState([]);
+  const { socket } = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on("online-users", (data) => {
+      setOnlineUsers(data.userList);
+    });
+
+    return () => {
+      socket.off("online-users");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("offline-users", (data) => {
+      setOfflineUsers(data.userList);
+    });
+
+    return () => {
+      socket.off("offline-users");
+    };
+  }, []);
+
   return (
     <div className="chat-list">
-      <ChatCard />
-      <ChatCard />
+      <ChatCard text="Public" hiddenCircle />
+      {onlineUsers.map((onlineUser) => (
+        <ChatCard
+          key={onlineUser.socketID}
+          text={onlineUser.nickname}
+          avatarSrc="https://randomuser.me/api/portraits/men/22.jpg"
+          online
+        />
+      ))}
+      {offlineUsers.map((offlineUser, index) => (
+        <ChatCard
+          key={index}
+          text={offlineUser.nickname}
+          avatarSrc={offlineUser.avatarURL}
+        />
+      ))}
     </div>
   );
 }
