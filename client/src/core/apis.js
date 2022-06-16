379c9524bb3b2a3c7190631f853d9a0e9f153204
Burak Yazan <brkyznn@gmail.com>
2022-06-16 23:57:22 +0300

    add config files for axios

--- client/src/core/apis.js
+++
@@ -1,7 +0,0 @@
-export const AUTH_VERIFY = '/user/authVerify';
-export const REGISTER = '/user/register';
-export const SET_AVATAR_IMG = '/user/setAvatar';
-export const GET_ROOM_LIST = '/user/getRoomList';
-export const GET_PUBLIC_MESSAGE_LIST = '/message/getPublicMessageList';
-export const GET_PRIVATE_MESSAGE_LIST = '/message/getPrivateMessageList';
-export const GET_ROOM_MESSAGE_LIST = '/message/getRoomMessageList';
--- client/src/core/firebase.js
+++ client/src/core/firebase.js
@@ -1,8 +1,8 @@
 import firebase from 'firebase';
-let fire;
+let firebaseAdmin;
 
 if (process.env.NODE_ENV === 'production') {
-  fire = firebase.initializeApp({
+  firebaseAdmin = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_CL_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_CL_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_CL_PROJECT_ID,
@@ -12,7 +12,9 @@ if (process.env.NODE_ENV === 'production') {
   });
 } else {
   const key = require('./serviceAccountKey');
-  fire = firebase.initializeApp(key.default);
+  firebaseAdmin = firebase.initializeApp(key.default);
 }
 
-export default fire;
+const auth = firebaseAdmin.auth();
+const database = firebaseAdmin.firestore();
+export {auth, database};
