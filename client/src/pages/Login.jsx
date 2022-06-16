833fb6349300afa42c4d416cd61ad2a51ebfa533
Burak Yazan <brkyznn@gmail.com>
2022-06-16 23:59:38 +0300

    add login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import { useState } from "react";
+import authService from "../services/authService";
+import { useDispatch } from "react-redux";
+import { changeNickname } from "../features/chatSlice";
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
 
 function Login() {
-  return (
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loader, setLoader] = useState(false);
+
+  const handleLoginClick = () => {
+    if (email.length === 0) {
+      console.error("email can not be empty!");
+      return;
+    }
+
+    if (password.length === 0) {
+      console.error("password can not be empty!");
+      return;
+    }
+
+    setLoader(true);
+    authService
+      .login(email, password)
+      .then((result) => {
+        localStorage.setItem("token", result.token);
+        dispatch(changeNickname(result.nickname));
+        navigate("/chat");
+      })
+      .catch((error) => {
+        console.error(error.code, error.message);
+        setLoader(false);
+      });
+  };
+
+  return loader ? (
+    <Loader open={loader} />
+  ) : (
     <div>
       <div>
         <label htmlFor="email">Email : </label>
-        <input type="text" name="email" />
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value.trim())}
+        />
       </div>
       <div>
         <label htmlFor="password">Password : </label>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value.trim())}
+        />
       </div>
       <div>
-        <button>Login</button>
+        <button onClick={handleLoginClick}>Login</button>
       </div>
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,26 +1,78 @@
-import React from "react";
+import { useState } from "react";
+import authService from "../services/authService";
+import Loader from "../components/Loader";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
-  return (
+  const navigate = useNavigate();
+
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loader, setLoader] = useState(false);
+
+  const handleRegisterButton = async () => {
+    if (email.length === 0) {
+      console.error("email can not be empty!");
+      return;
+    }
+
+    if (nickname.length === 0) {
+      console.error("nickname can not be empty!");
+      return;
+    }
+
+    if (nickname.length > 10) {
+      console.error("nickname length can not be more than ten!");
+      return;
+    }
+
+    setLoader(true);
+    authService
+      .register(nickname, email, password)
+      .then((result) => {
+        console.log(result.message);
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.error(error.code, error.message);
+        setLoader(false);
+      });
+  };
+
+  return loader ? (
+    <Loader open={loader} />
+  ) : (
     <div>
       <div>
+        <label htmlFor="nickname">Nickname : </label>
+        <input
+          type="text"
+          name="nickname"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+      </div>
+      <div>
         <label htmlFor="email">Email : </label>
-        <input type="text" name="email" />
-      </div>
-      <div>
-        <label htmlFor="email">First Name : </label>
-        <input type="text" name="firstName" />
-      </div>
-      <div>
-        <label htmlFor="email">Last Name : </label>
-        <input type="text" name="lastName" />
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password : </label>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div>
-        <button>Register</button>
+        <button onClick={handleRegisterButton}>Register</button>
       </div>
     </div>
   );
